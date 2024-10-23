773542841a4c0bd6e51ed5e0ec55ef65ec31657a
Darsha Anuradha <darsha.anuradha2020@gmail.com>
2024-10-23 20:24:51 +0530

    done

---
+++ resources/views/livewire/count-card.blade.php
@@ -0,0 +1,29 @@
+<div class="mb-1">
+    <div  class="text-2xl font-bold text-white bg-gradient-to-r {{ $color }} p-2 rounded-xl flex gap-2 shadow-xl">
+        <i  class="{{ $iconName }}"></i> 
+        {{ $cardName }} <span id="cardCount" > {{ $userCount }}</span>
+    </div>
+    
+    <script>
+        document.addEventListener('livewire:load', function () {
+            let count = 0;
+            let targetCount = @this.targetCount; // Livewire's reactive property
+            let speed = 1; // Adjust speed here for faster or slower animation
+    
+            const countLabel = document.getElementById('cardCount');
+    
+            const counter = setInterval(function() {
+                if (count < targetCount) {
+                    count++;
+                    countLabel.textContent = count;
+    
+                    // Update the Livewire property to maintain reactivity
+                    @this.set('userCount', count);
+                } else {
+                    clearInterval(counter);
+                }
+            }, speed);
+        });
+    </script>
+    
+</div>
