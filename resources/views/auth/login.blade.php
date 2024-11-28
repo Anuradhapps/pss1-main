c6d988db9a8102d01f60f69246cd08b850e65720
Anuradhapps <162093163+Anuradhapps@users.noreply.github.com>
2024-11-28 13:59:07 +0530

    done

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,22 +1,27 @@
 <x-guest-layout>
 @section('title', 'Login')
 <x-auth-card>
-
-	<x-form action="{{ route('login') }}">
-
+    <div class="flex justify-between">
+        <a href="{{ route('password.request') }}">Don't have an account?</a>
+        @if (Route::has('register'))
+            <a class="px-3 py-1 bg-green-600 rounded-lg hover:bg-green-700 hover:shadow-xl" href="{{ route('register') }}">Register</a>
+        @endif
+    </div>
+	<x-form class="mt-3" action="{{ route('login') }}">
+        <p class="mb-3">If you have an account, please login</p>
 		@include('errors.messages')
 
 		<x-form.input name="email" label="Email">{{ old('email') }}</x-form.input>
 		<x-form.input name="password" label="Password" type="password" />
 
 		<div class="flex justify-between">
-			<a href="{{ route('password.request') }}">Forgot your password?</a>
+			<a href="{{ route('password.request') }}"></a>
 			@if (Route::has('register'))
-                <a href="{{ route('register') }}">Register</a>
+                <a href="{{ route('register') }}"></a>
             @endif
 		</div>
 
-		<p><button type="submit" class="justify-center w-full btn btn-primary">Login</button></p>
+		<p><button type="submit" class="justify-center w-full px-4 py-2 text-sm text-white bg-blue-500 rounded shadow-md hover:bg-blue-700 hover:shadow-lg">Login</button></p>
 
 	</x-form>
 
