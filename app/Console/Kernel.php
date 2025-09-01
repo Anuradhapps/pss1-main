3fc113256ef1fc9ed4d9bbcbbe5ed8f2843f2e38
Anuradhapps <162093163+Anuradhapps@users.noreply.github.com>
2025-09-01 16:04:43 +0530

    done

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,8 +24,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('warnings:check')->dailyAt('08:00');
-        // $schedule->command('warnings:check')->everyMinute();
+        // $schedule->command('warnings:check')->dailyAt('08:00');
+        $schedule->command('warnings:check')->everyMinute();
     }
 
     /**
